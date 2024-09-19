aa4ae391d7a74b503df0bd3bdb642072b4c647b1
fatiyaa <fatiyaizzati26@gmail.com>
2024-09-19 13:32:55 +0700

    add model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -12,50 +14,13 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        ['id' => '1',
-        'title' => 'Judul Artikel 1', 
-        'slug' => 'judul-artikel-1',
-        'author' => 'Fatiya Izzati', 
-        'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex suscipit magnam cumque eum, totam harum amet, qui nam nobis exercitationem voluptas. Delectus, tempore. Aspernatur consequatur optio dignissimos
-            distinctio odit odio.'],
-        ['id' => '2',
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2', 
-        'author' => 'Fatiya Izzati', 
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum tempore doloribus ullam quis voluptatem ratione vel consequuntur. Labore enim reiciendis, exercitationem consequatur ipsa ratione expedita iste obcaecati harum quos tempore?'],
-        ['id' => '3',
-        'slug' => 'judul-artikel-3',
-        'title' => 'Judul Artikel 3', 
-        'author' => 'Fatiya Izzati', 
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod necessitatibus voluptatibus at aut delectus sunt rem repudiandae, consequatur ex magnam nam tenetur accusamus voluptates maiores suscipit. Praesentium, sit fugiat.'],
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        ['id' => '1',
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel 1', 
-        'author' => 'Fatiya Izzati', 
-        'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex suscipit magnam cumque eum, totam harum amet, qui nam nobis exercitationem voluptas. Delectus, tempore. Aspernatur consequatur optio dignissimos
-            distinctio odit odio.'],
-        ['id' => '2',
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2', 
-        'author' => 'Fatiya Izzati', 
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum tempore doloribus ullam quis voluptatem ratione vel consequuntur. Labore enim reiciendis, exercitationem consequatur ipsa ratione expedita iste obcaecati harum quos tempore?'],
-        ['id' => '3',
-        'slug' => 'judul-artikel-3',
-        'title' => 'Judul Artikel 3', 
-        'author' => 'Fatiya Izzati', 
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod necessitatibus voluptatibus at aut delectus sunt rem repudiandae, consequatur ex magnam nam tenetur accusamus voluptates maiores suscipit. Praesentium, sit fugiat.'],
-    ];
 
-    $post = Arr::first($posts, function($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
 
     return view('post', ['title' => 'Single Post', 'post' => $post] );
 
