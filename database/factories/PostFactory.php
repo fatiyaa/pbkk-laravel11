2c91999ea3a0ac21a86c13a7de315f623b536b87
fatiyaa <fatiyaizzati26@gmail.com>
2024-10-03 13:45:02 +0700

    eloquent relationship

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,8 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -19,7 +20,7 @@ class PostFactory extends Factory
     {
         return [
             'title' => fake()->sentence(),
-            'author' => fake()->name(),
+            'author_id' => User::factory(),
             'slug' => Str::slug(fake()->sentence()),
             'body' => fake()->text(),
         ];
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+Use App\Models\User;
 
 
 Route::get('/', function () {
@@ -25,6 +26,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     // return view('post', ['title' => 'Single Post']);
 });
 
+Route::get('/authors/{user}', function (User $user) {
+
+    return view('posts', ['title' => 'Articles by' . $user->name, 'posts' => $user->posts] );
+
+    // return view('post', ['title' => 'Single Post']);
+});
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
